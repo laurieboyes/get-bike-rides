b82709d2006aa4088754344aa1a53778024f4110
Laurie Boyes <laurie.boyes@ft.com>
2017-04-17 17:51:16 +0100

    more tests for numberArrayFuzzyFindIndex

--- test/util/number-array-fuzzy-find-index.spec.js
+++ test/util/number-array-fuzzy-find-index.spec.js
@@ -3,9 +3,33 @@ import { expect } from 'chai';
 import numberArrayFuzzyFindIndex from '../../src/util/number-array-fuzzy-find-index';
 
 describe('numberArrayFuzzyFindIndex', () => {
-	it('return the array index of the closest number to that which is given', () => {
+	it('should return the array index of the closest number to that which is given', () => {
 		const theArray = [3,8,30,60];
 		const theNumber = 16;
 		expect(numberArrayFuzzyFindIndex(theArray, theNumber)).to.equal(1);
+	});
+
+	it('should work when the number is the last in the array', () => {
+		const theArray = [3,8,30,60];
+		const theNumber = 200;
+		expect(numberArrayFuzzyFindIndex(theArray, theNumber)).to.equal(3);
+	});
+
+	it('should work when the number is the first in the array', () => {
+		const theArray = [3,8,30,60];
+		const theNumber = 1;
+		expect(numberArrayFuzzyFindIndex(theArray, theNumber)).to.equal(0);
+	});
+
+	it('should work for arrays with negative numbers too', () => {
+		const theArray = [-500, -56, 3,8,30,60];
+		const theNumber = -30;
+		expect(numberArrayFuzzyFindIndex(theArray, theNumber)).to.equal(1);
+	});
+
+	it('should work regardless of the order of the array', () => {
+		const theArray = [8, -500, 30, -56, 60, 3];
+		const theNumber = -30;
+		expect(numberArrayFuzzyFindIndex(theArray, theNumber)).to.equal(3);
 	});
 });
